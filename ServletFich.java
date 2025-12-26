dbff91aacc3561ad0053ad93578848dadafb23b1
jmm005 <juan.martinez.martinez@gregoriofer.com>
2025-12-26 11:34:55 +0100

    AccesoDatosA.jsp añadio y

--- ServletFich.java
+++ ServletFich.java
@@ -29,27 +29,34 @@ public class ServletFich extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String mensaje = "";
 		String envio = "";
+		String dato1 = request.getParameter("dato1");
+		String dato2 = request.getParameter("dato2");
+		String dato3 = request.getParameter("dato3");
+		String dato4 = request.getParameter("dato4");
+		String dato5 = request.getParameter("dato5");
+		String dato6 = request.getParameter("dato6");
 
 		if (request.getParameter("accion").equals("escritura")) {
-			if (request.getParameter("dato1").isEmpty() && request.getParameter("dato2").isEmpty()
-					&& request.getParameter("dato3").isEmpty() && request.getParameter("dato4").isEmpty()
-					&& request.getParameter("dato5").isEmpty() && request.getParameter("dato6").isEmpty()) {
+			if (dato1.isEmpty() || dato2.isEmpty() || dato3.isEmpty() || dato4.isEmpty() || dato5.isEmpty()
+					|| dato6.isEmpty()) {
 				mensaje = "(*) Los campos no pueden estar vacíos";
 				envio = "TratamientoFich.jsp";
 				request.setAttribute("mensaje", mensaje);
+				request.getRequestDispatcher(envio).forward(request, response);
+			}else {
+				request.setAttribute("dato1", dato1);
+				request.setAttribute("dato2", dato2);
+				request.setAttribute("dato3", dato3);
+				request.setAttribute("dato4", dato4);
+				request.setAttribute("dato5", dato5);
+				request.setAttribute("dato6", dato6);
+				envio = "AccesoDatosA.jsp";
 				request.getRequestDispatcher(envio).forward(request, response);
 			}
 		} else {
 			mensaje = "Lectura del fichero correcta";
 			request.setAttribute("mensaje", mensaje);
 		}
-
-		response.setContentType("text/html;charset=UTF-8");
-		response.getWriter().append("<html><head><title>Resultado</title></head><body>")
-							.append("<h2>" + mensaje + "</h2>")
-							.append("</body></html>");
-		response.getWriter().close();
-
 	}
 
 	/**
